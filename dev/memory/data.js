be32771e4bc55b709e77791ee817b8d1364e3a45
github-action-benchmark <github@users.noreply.github.com>
2026-07-26 14:13:14 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for b10603ea22fa2b04172a49083b18740bf808e75a

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784993649973,
+  "lastUpdate": 1785075194851,
   "repoUrl": "https://github.com/shuvroroy/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -33,6 +33,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 72.2,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "b10603ea22fa2b04172a49083b18740bf808e75a",
+          "message": "Switch all linux release builds to musl + mimalloc, add memory profiling\nfeatures, plan memory optimizations",
+          "timestamp": "2026-07-26T05:17:44+02:00",
+          "tree_id": "91d1f3303b0c2295947eeb95439dcb8bf9ca66d6",
+          "url": "https://github.com/shuvroroy/phpantom_lsp/commit/b10603ea22fa2b04172a49083b18740bf808e75a"
+        },
+        "date": 1785075194048,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 33.7,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 73.2,
             "unit": "MiB"
           }
         ]
