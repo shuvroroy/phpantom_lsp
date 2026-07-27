32f236cc60aad9cacc7261515873cb9262b1fb10
github-action-benchmark <github@users.noreply.github.com>
2026-07-27 16:42:49 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 579bce936bc3490a19aba6487b7bc7b46a87cce9

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785075194851,
+  "lastUpdate": 1785170569466,
   "repoUrl": "https://github.com/shuvroroy/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -67,6 +67,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 73.2,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "579bce936bc3490a19aba6487b7bc7b46a87cce9",
+          "message": "Type narrowing against `@phpstan-assert`/`@psalm-assert` no longer leaks\nmemory",
+          "timestamp": "2026-07-27T14:59:08+02:00",
+          "tree_id": "c186c899c7211dd4d6d2eec0cd8753aaa3e86f63",
+          "url": "https://github.com/shuvroroy/phpantom_lsp/commit/579bce936bc3490a19aba6487b7bc7b46a87cce9"
+        },
+        "date": 1785170568567,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 33.3,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 71.3,
             "unit": "MiB"
           }
         ]
